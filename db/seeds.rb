b7f0f58c999cb6c9b2fceb65be700b9e302adfae
Matthew Meakin <meakin.matt@gmail.com>
2025-05-22 15:44:36 -0500

    added movie posters

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+require 'faker'
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+puts "Seeding movies..."
+
+10.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.paragraph(sentence_count: 3),
+    rating: rand(6.0..9.5).round(1),
+    poster_url: "https://loremflickr.com/320/480/movie,japan?lock=#{rand(1..1000)}"
+  )
+end
+
+puts "Seeding lists and bookmarks..."
+
+list = List.create!(name: "Japanese Horror Picks")
+
+Movie.all.each do |movie|
+  Bookmark.create!(
+    movie: movie,
+    list: list,
+    comment: Faker::Quote.jack_handey
+  )
+end
+
+puts "Done seeding 🎬🩸"
